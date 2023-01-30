6e1163deeddb9ba665a4d8b2131c1333c47509e2
regennur <93402952+regennur@users.noreply.github.com>
2023-01-30 21:48:37 +0300

    Update get_rifma.py

--- get_rifma.py
+++ get_rifma.py
@@ -1,12 +1,27 @@
 import requests
-
+from flask import Flask, render_template, jsonify
+import requests
+from bs4 import BeautifulSoup
 
 def getRif(word="кот"):
-    res = []
-    response = requests.get('https://py-data-scale.herokuapp.com/w/' + word)
-    if response.status_code == 200:
-        res_t = response.json()
-        if 'data' in res_t:
-            res = res_t['data']
+    res_t = show_word(word)
+    if 'data' in res_t:
+        res = res_t['data']
 
     return res
+
+def show_word(word):
+    response = requests.get('https://rifmu.ru/' + word)
+    contents_html = response.content
+
+    soup = BeautifulSoup(contents_html, 'lxml')
+    text_arr = []
+    if soup.ul == None: 
+        return  {"data": []}
+    for child in soup.ul.recursiveChildGenerator():
+
+        if child.name == 'a':
+
+            text_arr.append(child.text)
+
+    return {"data": text_arr}
